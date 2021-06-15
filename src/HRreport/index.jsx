c97bb0b94b8cbc84dc24b9d9ffa49179eb07d053
olebjorn <olebgran@hotmail.com>
2021-06-15 17:59:20 +0200

    added firebase

--- src/HRreport/index.jsx
+++ src/HRreport/index.jsx
@@ -6,7 +6,7 @@ const Report = () => {
     return (
         <div>
             {/*<input type="text" value={inputText} onChange={e => setInputText(e.target.value)} />*/}
-            <textarea rows="10" cols="25" name="description" onChange={e => setInputText(e.target.value)}></textarea><br/>
+            <textarea style={{resize: "none"}} rows="10" cols="25" name="description" onChange={e => setInputText(e.target.value)}></textarea><br/>
             
             <button type="button" onClick={() => console.log(inputText)}> Submit</button><br/>
             <br/><button onClick={() => console.log("Done")}>Done</button>
