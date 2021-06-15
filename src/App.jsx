4ccf01df7d325b14cfeb3acc54a35ebc4df4919f
olebjorn <olebgran@hotmail.com>
2021-06-15 17:26:08 +0200

    added logo, updated ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.css';
 import React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import report from "./HRreport"
+import admin from "./HRadmin"
 
 function App() {
   return (
     <Router>
+      <img src={process.env.PUBLIC_URL + "/qualisoft.png"} width="100" alt="logo" />
+      <h2>Qualisoft HR reports</h2>
       <Switch>
-    {/* <Route path="/:id" component={RoomAdmin} /> */}
+    {/* <Route path="/:id" component={admin} /> */}
         <Route exact path="/" component={report} />
       </Switch>
     </Router>
--- src/HRreport/index.jsx
+++ src/HRreport/index.jsx
@@ -5,8 +5,11 @@ const Report = () => {
     const [inputText, setInputText] = React.useState("");
     return (
         <div>
-            <input type="text" value={inputText} onChange={e=>setInputText(e.target.value)}/>
-            <button type="button" onClick={()=> console.log(inputText)}> Click Me!</button>
+            {/*<input type="text" value={inputText} onChange={e => setInputText(e.target.value)} />*/}
+            <textarea rows="10" cols="25" name="description" onChange={e => setInputText(e.target.value)}></textarea><br/>
+            
+            <button type="button" onClick={() => console.log(inputText)}> Submit</button><br/>
+            <br/><button onClick={() => console.log("Done")}>Done</button>
         </div>
     )
 }
